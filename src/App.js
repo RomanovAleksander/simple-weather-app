a0bd905e438ca23cb133b6484f9832fce9114f83
RomanovAleksander <1eksandr999@gmail.com>
2021-10-14 17:42:18 +0300

    geolocation request added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React  from 'react';
+import React, { useState, useEffect }  from 'react';
 
 function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    })
+  }, []);
+
   return (
     <div className="App">
       App
